57f70154ccc5039b70807e53542b69e3387ea6b3
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-29 10:24:30 -0700

    abstracted to project card component

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="card">
+    <img :src="project.coverImg" class="card-img-top" :alt="project.title">
+    <div class="card-body d-flex justify-content-between">
+      <div>
+        <p class="card-title fs-5">{{ project.title }}</p>
+        <button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#projectModal">Open Gallery</button>
+      </div>
+      <img class="creator-picture" :src="project.creator.picture" :alt="project.creator.name">
+    </div>
+  </div>
+</template>
+
+
+<script>
+import { Project } from '../models/Project.js';
+
+export default {
+  props: {
+    project: { type: Project, required: true }
+  },
+  setup() {
+    return {}
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+.creator-picture {
+  height: 8vh;
+  aspect-ratio: 1/1;
+  border-radius: 50%;
+  object-fit: cover;
+}
+
+.card-img-top {
+  height: 32vh;
+  object-fit: cover;
+}
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -8,16 +8,7 @@
 
     <section class="row">
       <div v-for="project in projects" :key="project.id" class="col-md-3 mb-3">
-        <div class="card">
-          <img :src="project.coverImg" class="card-img-top" :alt="project.title">
-          <div class="card-body d-flex justify-content-between">
-            <div>
-              <p class="card-title fs-5">{{ project.title }}</p>
-              <button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#projectModal">Open Gallery</button>
-            </div>
-            <img class="creator-picture" :src="project.creator.picture" :alt="project.creator.name">
-          </div>
-        </div>
+        <ProjectCard :project="project" />
       </div>
     </section>
   </div>
@@ -30,39 +21,27 @@ import { computed, onMounted } from 'vue';
 import Pop from '../utils/Pop.js';
 import { projectsService } from '../services/ProjectsService.js'
 import { AppState } from '../AppState.js'
+import ModalComponent from '../components/ModalComponent.vue';
 export default {
   setup() {
-
     async function getProjects() {
       try {
-        await projectsService.getProjects()
-      } catch (error) {
-        Pop.error(error)
+        await projectsService.getProjects();
+      }
+      catch (error) {
+        Pop.error(error);
       }
     }
-
     onMounted(() => {
       console.log('home page mounted');
-      getProjects()
-    })
-
+      getProjects();
+    });
     return {
       projects: computed(() => AppState.projects)
-    }
-  }
+    };
+  },
+  components: { ModalComponent }
 }
 </script>
 
-<style scoped lang="scss">
-.creator-picture {
-  height: 8vh;
-  aspect-ratio: 1/1;
-  border-radius: 50%;
-  object-fit: cover;
-}
-
-.card-img-top {
-  height: 32vh;
-  object-fit: cover;
-}
-</style>
+<style scoped lang="scss"></style>
