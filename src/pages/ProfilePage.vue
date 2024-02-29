cc02c2aea318815101fa4684647cdbfb53a72dec
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-29 11:31:08 -0700

    got profile by id

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -4,8 +4,29 @@
 
 
 <script>
+import { onMounted } from 'vue';
+import { logger } from '../utils/Logger.js';
+import Pop from '../utils/Pop.js';
+import { profilesService } from '../services/ProfilesService.js'
+import { useRoute } from 'vue-router';
+
 export default {
   setup() {
+    const route = useRoute()
+
+    async function getProfileById() {
+      try {
+        const profileId = route.params.profileId
+        logger.log('Id from route parameters', profileId)
+        await profilesService.getProfileById(profileId)
+      } catch (error) {
+        Pop.error(error)
+      }
+    }
+
+    onMounted(() => {
+      getProfileById()
+    })
     return {}
   }
 }
---
+++ src/services/ProfilesService.js
@@ -0,0 +1,11 @@
+import { logger } from "../utils/Logger.js"
+import { api } from "./AxiosService.js"
+
+class ProfilesService {
+  async getProfileById(profileId) {
+    const response = await api.get(`api/profiles/${profileId}`)
+    logger.log('GOT PROFILE', response.data)
+  }
+}
+
+export const profilesService = new ProfilesService()
